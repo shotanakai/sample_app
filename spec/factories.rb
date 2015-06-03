531a9b92b4745abd30b26d05c60d7c92bf769bfc
Shota Nakai <newgenenewera@gmail.com>
2015-06-03 17:33:35 +0900

    Finish user edit, update, index, and destroy action

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
 	factory :user do
-		name	"Shota Nakai"
-		email	"hoge@hoge.com"
+		sequence(:name)	{ |n| "Person#{n}" }
+		sequence(:email)	{ |n| "Person#{n}@example.com"}
 		password	"hogehoge"
 		password_confirmation	"hogehoge"
+
+		factory :admin do
+			admin true
+		end
 	end	
 end
